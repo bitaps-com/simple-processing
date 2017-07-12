97c8fca71a8f8f4a5e0245c538c38a9990cbaeef
admin <admin@bitaps.com>
2017-07-12 18:03:54 +0300

    init

---
+++ processing.py
@@ -0,0 +1,170 @@
+import asyncio
+import argparse
+import aiomysql
+import traceback
+import colorlog
+from pythonjsonlogger import jsonlogger
+import sys
+import aiomysql
+import configparser
+import logging
+import zmq
+import zmq.asyncio
+import struct
+import binascii
+import aiojsonrpc
+from zlib import crc32
+
+class App():
+    def __init__(self, loop, logger, config):
+        print("test")
+        self.loop = loop
+        self.log = logger
+        self.zmq_url = config["BITCOIND"]["zeromq"]
+        self.zmqContext = zmq.asyncio.Context()
+        self.zmqSubSocket = self.zmqContext.socket(zmq.SUB)
+        self.MYSQL_CONFIG = config["MYSQL"]
+        # self.zmqSubSocket.setsockopt_string(zmq.SUBSCRIBE, "hashblock")
+        # self.zmqSubSocket.setsockopt_string(zmq.SUBSCRIBE, "hashtx")
+        # self.zmqSubSocket.setsockopt_string(zmq.SUBSCRIBE, "rawblock")
+        self.zmqSubSocket.setsockopt_string(zmq.SUBSCRIBE, "rawtx")
+        self.zmqSubSocket.connect(self.zmq_url)
+        print(self.zmq_url)
+        # self.loop.create_task(self.handle())
+        # self.loop.create_task(self.rpctest())
+        self.loop.create_task(self.mysqltest())
+
+    async def handle(self) :
+        msg = await self.zmqSubSocket.recv_multipart()
+        topic = msg[0]
+        body = msg[1]
+        sequence = "Unknown"
+        if len(msg[-1]) == 4:
+          msgSequence = struct.unpack('<I', msg[-1])[-1]
+          sequence = str(msgSequence)
+        if topic == b"hashblock":
+            print('- HASH BLOCK ('+sequence+') -')
+            print(binascii.hexlify(body))
+        elif topic == b"hashtx":
+            print('- HASH TX  ('+sequence+') -')
+            print(binascii.hexlify(body))
+        elif topic == b"rawblock":
+            print('- RAW BLOCK HEADER ('+sequence+') -')
+            print(binascii.hexlify(body))
+        elif topic == b"rawtx":
+            print('- RAW TX ('+sequence+') -')
+            print(binascii.hexlify(body))
+        # schedule ourselves to receive the next message
+        asyncio.ensure_future(self.handle())
+
+    async def rpctest(self):
+        self.rpc = aiojsonrpc.rpc(self.config["BITCOIND"]["rpc"], loop)
+        while 1:
+            p = await  self.rpc.getinfo()
+            print(p)
+            await asyncio.sleep(10)
+
+    async def mysqltest(self):
+        conn = await \
+            aiomysql.connect(user=self.MYSQL_CONFIG["user"],
+                             password=self.MYSQL_CONFIG["password"],
+                             db="",
+                             host=self.MYSQL_CONFIG["host"],
+                             port=int(self.MYSQL_CONFIG["port"]),
+                             loop=self.loop)
+        cur = await conn.cursor()
+        # await init_db(self.MYSQL_CONFIG["database"], cur)
+        conn.close()
+
+
+def init(loop, argv):
+    parser = argparse.ArgumentParser(description="Simple processing server  v 0.0.1")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-c", "--config", help="config file", type=str, nargs=1, metavar=('PATH',))
+    group.add_argument("-l", "--log", help="log file", type=str, nargs=1, metavar=('PATH',))
+    parser.add_argument("-v", "--verbose", help="increase output verbosity", action="count", default=0)
+    parser.add_argument("--json", help="json formatted logs", action='store_true')
+    args = parser.parse_args()
+    config_file = "simple-processing.cnf"
+    log_file = "somple-processing.log"
+    log_level = logging.WARNING
+    logger = colorlog.getLogger('sp')
+    if args.config is not None:
+        config_file = args.config
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    if args.log is None:
+        if "LOG" in config.sections():
+            if "log_file" in config['LOG']:
+                log_file = config['LOG']["log_file"]
+            if "log_level" in config['LOG']:
+                if config['LOG']["log_level"] == "info":
+                    log_level = logging.INFO
+                elif config['LOG']["log_level"] == "info":
+                    log_level = logging.INFO
+                elif config['LOG']["log_level"] == "debug":
+                    log_level = logging.DEBUG
+
+    else:
+        log_file = args.log
+    if args.verbose == 0:
+        log_level = logging.WARNING
+    elif args.verbose == 1:
+        log_level = logging.INFO
+    elif args.verbose > 1:
+        log_level = logging.DEBUG
+        if args.verbose > 3:
+            connector_debug = True
+        if args.verbose > 4:
+            connector_debug = True
+            connector_debug_full = True
+    if log_level == logging.WARNING and "LOG" in config.sections():
+        if "log_level" in config['LOG']:
+            if config['LOG']["log_level"] == "info":
+                log_level = logging.INFO
+            elif config['LOG']["log_level"] == "debug":
+                log_level = logging.DEBUG
+
+    if args.json:
+        logger = logging.getLogger()
+        logHandler = logging.StreamHandler()
+        formatter = jsonlogger.JsonFormatter('%(created)s %(asctime)s %(levelname)s %(message)s %(module)s %(lineno)d)')
+        logHandler.setFormatter(formatter)
+        logger.addHandler(logHandler)
+        logger.setLevel(log_level)
+    else:
+        logger.setLevel(log_level)
+        logger.debug("test")
+        fh = logging.FileHandler(log_file)
+        fh.setLevel(log_level)
+        ch = logging.StreamHandler()
+        ch.setLevel(log_level)
+        formatter = colorlog.ColoredFormatter('%(log_color)s%(asctime)s %(levelname)s: %(message)s (%(module)s:%(lineno)d)')
+        fh.setFormatter(formatter)
+        ch.setFormatter(formatter)
+        logger.addHandler(fh)
+        logger.addHandler(ch)
+    try:
+        config["BITCOIND"]["zeromq"]
+        config["BITCOIND"]["rpc"]
+    except Exception as err:
+        print(traceback.format_exc())
+        logger.critical("Bitcoind config failed: %s" % err)
+        logger.critical("Shutdown")
+        sys.exit(0)
+
+    logger.setLevel(log_level)
+    logger.info("Start")
+
+    loop = asyncio.get_event_loop()
+    app = App(loop, logger, config)
+    return app
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop = zmq.asyncio.install()
+    app = init(loop, sys.argv[1:])
+    loop.run_forever()
+    pending = asyncio.Task.all_tasks()
+    loop.run_until_complete(asyncio.gather(*pending))
+    loop.close()
---
+++ utils.py
@@ -0,0 +1,48 @@
+b58_digits = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+
+def encode_Base58(b):
+    """Encode bytes to a base58-encoded string"""
+    # Convert big-endian bytes to integer
+    n = int('0x0' + binascii.hexlify(b).decode('utf8'), 16)
+    # Divide that integer into bas58
+    res = []
+    while n > 0:
+        n, r = divmod(n, 58)
+        res.append(b58_digits[r])
+    res = ''.join(res[::-1])
+    # Encode leading zeros as base58 zeros
+    czero = 0
+    pad = 0
+    for c in b:
+        if c == czero:
+            pad += 1
+        else:
+            break
+    return b58_digits[0] * pad + res
+
+
+def decode_Base58(s):
+    """Decode a base58-encoding string, returning bytes"""
+    if not s:
+        return b''
+    # Convert the string to an integer
+    n = 0
+    for c in s:
+        n *= 58
+        if c not in b58_digits:
+            raise Exception('Character %r is not a valid base58 character' % c)
+        digit = b58_digits.index(c)
+        n += digit
+    # Convert the integer to bytes
+    h = '%x' % n
+    if len(h) % 2:
+        h = '0' + h
+    res = binascii.unhexlify(h.encode('utf8'))
+    # Add padding back.
+    pad = 0
+    for c in s[:-1]:
+        if c == b58_digits[0]:
+            pad += 1
+        else:
+            break
+    return b'\x00' * pad + res
